5342d18538599d41fe9750021066c3d41518c0ff
jeveen <jeveenj@interrait.com>
2012-10-08 09:11:42 +0530

    WSO2 ESB install script for AppDirector

---
+++ ESB-Install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -e
+
+################## INTERNAL PARAMETERS #########################
+DOWNLOAD_URL="$download_url"
+INSTALL_DIR="$install_dir"
+
+################INSTALLING WSO2##############################
+echo "Installing Pre-requistes"
+if [ -f /etc/redhat-release ] ; then
+   echo "RHEL / CENTOS OS"
+   yum --nogpgcheck -y install unzip
+elif [ -f /etc/debian_version ] ; then
+   echo "Ubuntu OS"   
+   apt-get -f -y install linux-firmware --fix-missing
+   apt-get -f -y install unzip
+elif [ -f /etc/SuSE-release ] ; then
+   echo "SUSE OS"
+   zypper --non-interactive --no-gpg-checks install unzip
+fi
+
+mkdir $INSTALL_DIR
+cd $INSTALL_DIR
+### Downloading tar ball
+echo "Downloading the tar ball"
+wget --output-document=installer.zip $DOWNLOAD_URL
+echo "Downloading the tar ball - DONE"
+
+### Extracting tar ball
+echo "Extracting the tar ball"
+unzip installer.zip
+echo "Extracting the tar ball - DONE"
